ae981404819fb9d653ecff2104daa6df920dadf1
Florin Chelaru <florin.chelaru@yahoo.com>
2016-02-16 18:14:47 -0500

    add demo visualization

---
+++ export/svg/demo.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Florin Chelaru ( florin [dot] chelaru [at] gmail [dot] com )
+ * Date: 2/16/2016
+ * Time: 5:21 PM
+ */
+
+
+goog.require('vs.ui.plugins.svg.Demo');
+
+goog.exportSymbol('vs.ui.plugins.svg.Demo', vs.ui.plugins.svg.Demo);
+goog.exportProperty(vs.ui.plugins.svg.Demo.prototype, 'endDraw', vs.ui.plugins.svg.Demo.prototype.endDraw);
---
+++ src/vs/ui/plugins/svg/demo.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Florin Chelaru ( florin [dot] chelaru [at] gmail [dot] com )
+ * Date: 2/16/2016
+ * Time: 5:21 PM
+ */
+
+
+goog.provide('vs.ui.plugins.svg.Demo');
+
+if (COMPILED) {
+  goog.require('vs.ui');
+}
+
+/**
+ * @constructor
+ * @extends vs.ui.svg.SvgVis
+ */
+vs.ui.plugins.svg.Demo = function() {
+  vs.ui.svg.SvgVis.apply(this, arguments);
+
+  /**
+   * @type {number}
+   */
+  this['publicField'] = 20;
+
+  /**
+   * @type {number}
+   * @private
+   */
+  this._privateField = 10;
+};
+
+goog.inherits(vs.ui.plugins.svg.Demo, vs.ui.svg.SvgVis);
+
+/**
+ * @override
+ * @returns {Promise}
+ */
+vs.ui.plugins.svg.Demo.prototype.endDraw = function() {
+  var self = this;
+  var args = arguments;
+  return new Promise(function(resolve, reject) {
+    /** @type {vs.models.DataSource} */
+    var data = self.data;
+
+    // Nothing to draw
+    if (!data.nrows || !data.ncols) { resolve(); return; }
+
+    var svg = d3.select(self.$element[0]).select('svg');
+
+    var viewport = svg.select('.viewport');
+    if (viewport.empty()) {
+      viewport = svg.append('g')
+        .attr('class', 'viewport');
+    }
+
+    viewport.append('circle')
+      .attr('cx', 50)
+      .attr('cy', 50)
+      .attr('r', 20)
+      .attr('fill', '#fe5621')
+      .style('stroke', '#000000')
+      .style('stroke-width', 5);
+
+    resolve();
+  }).then(function() {
+    return vs.ui.svg.SvgVis.prototype.endDraw.apply(self, args);
+  });
+};
