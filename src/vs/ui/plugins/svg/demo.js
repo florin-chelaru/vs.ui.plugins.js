4888726ddf2f9126dfb32be6371a7b330e05aa26
John La <johnphi4747@gmail.com>
2016-03-01 16:23:50 -0500

    Fixed duplicate demo

--- src/vs/ui/plugins/svg/demo.js
+++ src/vs/ui/plugins/svg/demo.js
@@ -49,38 +49,4 @@ vs.ui.plugins.svg.Demo.prototype.endDraw = function() {
     }).then(function(){
         return vs.ui.svg.SvgVis.prototype.endDraw.apply(self, args);
     });
-}; /**
- *@override
- *@returns{Promise}
- */
-vs.ui.plugins.svg.Demo.prototype.endDraw = function() {
-    var self = this;
-    var args = arguments;
-    return new Promise(function(resolve, reject) {
-        /**@type{vs.models.DataSource}*/
-        var data = self.data;
-
-        // Nothing to draw
-        if (!data.nrows || !data.ncols){ resolve(); return; }
-
-        var svg = d3.select(self.$element[0]).select('svg');
-
-        var viewport = svg.select('.viewport');
-        if (viewport.empty()) {
-            viewport = svg.append('g')
-                .attr('class', 'viewport');
-        }
-
-        viewport.append('circle')
-            .attr('cx',50)
-            .attr('cy',50)
-            .attr('r',20)
-            .attr('fill','#fe5621')
-            .style('stroke','#000000')
-            .style('stroke-width',5);
-
-        resolve();
-    }).then(function(){
-        return vs.ui.svg.SvgVis.prototype.endDraw.apply(self, args);
-    });
-};
+}; 
