a69d2973179347de92c81e92220a4b7b8abc4dd5
John La <johnphi4747@gmail.com>
2016-03-03 19:11:35 -0500

    one line

--- vs.ui.plugins.js
+++ vs.ui.plugins.js
@@ -532,9 +532,9 @@ vs.ui.plugins.svg.Line.prototype.endDraw = function() {
 
 
         var items = data.asDataRowArray();
-        var selection = vis.selectAll('path').data(items, vs.models.DataSource.key);
+        var selection = vis.select('path').data(items, vs.models.DataSource.key);
         selection.enter()
-            //.append('path')
+            .append('path')
             .attr('d', d3.svg.line()
                 .x(function (d) {
                     return xScale(parseFloat(d.info(row)));
